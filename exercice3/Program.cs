8a9b3cf3e5a9dfc69fa17fb780cd7e4f573984c8
Philippe Kurz <philippe.kurz@gmail.com>
2024-01-08 14:41:59 +0100

    exercice 4

--- exercice3/Program.cs
+++ exercice3/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Type texte
+string texte = "Bonjour tout le monde"; // Chaîne de caractères
+// Caractère spéciaux
+string texte = "Bonjour \"tout\" \nle monde"; // Chaîne de caractères
+
+// Concaténation
+string texte = "Bonjour " + "tout " + "le monde"; // Chaîne de caractères
+
+// Interpolation
+string prenom = "Pierre";
+string texte = $"Bonjour {prenom}"; // Chaîne de caractères -> Bonjour Pierre
+
+// Type char
+char lettre = 'a'; // Caractère
