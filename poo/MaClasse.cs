e3d5d12569e9e1e555403fc58247a831b2d8b5d3
Philippe Kurz <philippe.kurz@gmail.com>
2024-01-22 11:35:50 +0100

    classes

---
+++ poo/MaClasse.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace poo
+{
+    public class MaClasse
+    {
+        public void MaMethode()
+        {
+            Console.WriteLine("MaMethode");
+        }
+
+        public void Ecrire(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public string Lire()
+        {
+            return Console.ReadLine();
+        }
+
+        public override string ToString()
+        {
+            return "MaClasse";
+        }
+    }
+}
---
+++ poo/Program.cs
@@ -0,0 +1,9 @@
+﻿using poo;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Classes !");
+
+MaClasse maClasse = new MaClasse();
+
+maClasse.MaMethode();   // Invoque la méthode MaMethode() de l'objet maClasse
+
